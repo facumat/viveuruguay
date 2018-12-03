a1e5a28ee91ab095be7af135c1786132d26a04fa
Facundo Matiaude <matiaudefacundo@gmail.com>
2018-12-02 21:47:41 -0300

    whatevs

---
+++ js/my-script.js
@@ -0,0 +1,10 @@
+
+$('.navbar-collapse>div>ul>li>a').on('click', function(){
+    console.log("Pepe")
+    $('.navbar-collapse').collapse('hide');
+});
+
+
+ScrollReveal().reveal('.derecha', { delay: 400 });
+ScrollReveal({ reset: true });
+
